bd6527508966a23e04368f45adec4ac6740bfb1f
Анна Кулакова <annakulakova72001@mail.ru>
2026-06-03 18:16:40 +0300

    Renewed dataset

--- src/data/species_list.py
+++ src/data/species_list.py
@@ -1,29 +1,36 @@
 TARGET_SPECIES = [
-    "Anthus pratensis",                # Луговой конёк             | 24
     "Serinus serinus",                 # Канареечный вьюрок        | 30
-    "Corvus cornix",                   # Серая ворона              | 36
+    "Turdus iliacus",                  # Белобровик                | 32
+    "Corvus cornix",                   # Серая ворона              | 36  
+    "Erithacus rubecula",              # Зарянка                   | 39
+    "Anthus pratensis",                # Луговой конёк             | 40
     "Linaria cannabina",               # Коноплянка                | 40
     "Columba livia",                   # Сизый голубь              | 46
     "Carpodacus erythrinus",           # Обыкновенная чечевица     | 47
     "Strix uralensis",                 # Длиннохвостая неясыть     | 47
     "Chloris chloris",                 # Зеленушка                 | 51
     "Circus pygargus",                 # Луговой лунь              | 52
+    "Parus major",                     # Большая синица            | 52
     "Passer domesticus",               # Домовой воробей           | 52
+    "Turdus pilaris",                  # Рябинник                  | 52
     "Passer montanus",                 # Полевой воробей           | 53
     "Pyrrhula pyrrhula",               # Снегирь                   | 56
     "Bombycilla garrulus",             # Свиристель                | 58
 
-   
+    "Aegithalos caudatus",             # Длиннохвостая синица      | 64
+    "Cyanistes caeruleus",             # Лазоревка                 | 68
     "Motacilla flava",                 # Жёлтая трясогузка         | 68  
     "Spinus spinus",                   # Чиж                       | 68
     "Coccothraustes coccothraustes",   # Дубонос                   | 72
     "Emberiza schoeniclus",            # Камышовая овсянка         | 72
     "Oenanthe oenanthe",               # Обыкновенная каменка      | 73  
+    "Dendrocopos leucotos",            # Белоспинный дятел         | 80
     "Chroicocephalus ridibundus",      # Озёрная чайка             | 96
     "Fringilla coelebs",               # Зяблик                    | 96  
     "Hydrocoloeus minutus",            # Малая чайка               | 96
     "Larus argentatus",                # Серебристая чайка         | 96  
     "Motacilla alba",                  # Белая трясогузка          | 100
+    "Troglodytes troglodytes",         # Крапивник                 | 100
     "Anthus trivialis",                # Лесной конёк              | 103
     "Certhia familiaris",              # Обыкновенная пищуха       | 106 
     "Emberiza citrinella",             # Обыкновенная овсянка      | 116
